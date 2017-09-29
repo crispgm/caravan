55cff97e78f83e2ebffbc8d65c93402b0cb46df5
David Zhang <crispgm@gmail.com>
2017-09-29 14:20:34 +0800

    Add message tests

---
+++ test/message_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class MessageTest < Minitest::Test
+  context "test message" do
+    should "print debug" do
+      assert_output("[DEBUG] debug message".light_black << "\n") do
+        Caravan::Message.debug("debug message")
+      end
+
+      assert_output("[INFO] info message\n") do
+        Caravan::Message.info("info message")
+      end
+
+      assert_output("[ERROR] error message".red << "\n") do
+        Caravan::Message.error("error message")
+      end
+
+      assert_output("[WARN] warn message".yellow << "\n") do
+        Caravan::Message.warning("warn message")
+      end
+
+      assert_output("succ message".green << "\n") do
+        Caravan::Message.success("succ message")
+      end
+    end
+  end
+end
