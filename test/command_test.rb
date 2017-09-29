dc47599f21f48eb408129e555a71aa71e0c4a19d
David Zhang <crispgm@gmail.com>
2017-09-29 14:12:08 +0800

    Add command tests

---
+++ test/command_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class CommandTest < Minitest::Test
+  context "run command" do
+    should "succeed and return 0" do
+      status, output = Caravan::Command.run("echo test-command")
+      assert_equal(0, status)
+      assert_equal("test-command\n", output)
+    end
+
+    should "not run inexisted command and return 127" do
+      status, output = Caravan::Command.run("sh ./scripts/inexisted.sh")
+      assert_equal(127, status)
+      assert_equal("", output)
+    end
+
+    should "run but fail and return 255" do
+      status, output = Caravan::Command.run("sh ./test/scripts/error_command.sh")
+      assert_equal(255, status)
+      assert_equal("there is an error\n", output)
+    end
+  end
+end
---
+++ test/scripts/error_command.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+echo "there is an error"
+exit 255
