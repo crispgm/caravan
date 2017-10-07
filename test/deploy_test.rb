0c39dc343e49917e991639612ece7928a91c368d
David Zhang <crispgm@gmail.com>
2017-10-07 19:53:59 +0800

    Add deploy test

---
+++ test/deploy_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class DeployTest < Minitest::Test
+  context "test deploy" do
+    should "create shell deployer" do
+      shell_1 = Caravan::Deploy.create_deployer()
+      shell_2 = Caravan::Deploy.create_deployer("shell")
+      assert_equal(true, shell_1.is_a?(Caravan::DeployMethods::Shell))
+      assert_equal(true, shell_2.is_a?(Caravan::DeployMethods::Shell))
+    end
+
+    should "create scp deployer" do
+      scp = Caravan::Deploy.create_deployer("scp")
+      assert_equal(true, scp.is_a?(Caravan::DeployMethods::Scp))
+    end
+
+    should "create rsync deployer" do
+      rsync = Caravan::Deploy.create_deployer("rsync")
+      assert_equal(true, rsync.is_a?(Caravan::DeployMethods::Rsync))
+    end
+
+    should "create rsync local deployer" do
+      rsync_local = Caravan::Deploy.create_deployer("rsync_local")
+      assert_equal(true, rsync_local.is_a?(Caravan::DeployMethods::RsyncLocal))
+    end
+
+    should "error if deployer not exists" do
+      assert_output("[ERROR] Unknown deploy method \"not_a_deployer\"".red << "\n") do
+        output = Caravan::Deploy.create_deployer("not_a_deployer")
+        assert_nil(output)
+      end
+    end
+  end
+end
