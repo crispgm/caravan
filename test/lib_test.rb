0112ebb71a6327b0e0976d4656b25c130da51a3d
David Zhang <crispgm@gmail.com>
2017-10-29 11:32:59 +0800

    Add test for lib func

---
+++ test/lib_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+require "listen"
+
+class LibTest < CaravanTest
+  context "caravan tests" do
+    should "create a listen" do
+      deployer = Caravan::Deploy.create_deployer("shell")
+      listener = Caravan.create_listener(deployer, ".", ".")
+      assert_equal(true, listener.is_a?(Listen::Listener))
+    end
+  end
+end
