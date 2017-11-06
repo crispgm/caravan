8688a7fa069d5c17d59f57c91749956636ccc518
David Zhang <crispgm@gmail.com>
2017-11-06 16:33:16 +0800

    Add setup & teardown funcs for each deployer's test

--- test/deploy_test.rb
+++ test/deploy_test.rb
@@ -6,49 +6,81 @@ class DeployTest < CaravanTest
   TARGET_FOLDER = "test/target_folder".freeze
 
   context "test deploy" do
-    setup do
-      Caravan::Command.run("mkdir -p #{TARGET_FOLDER}")
-    end
-
-    teardown do
-      Caravan::Command.run("rm -rf #{TARGET_FOLDER}")
-    end
-
     should "create shell deployer by default" do
       deployer = Caravan::Deploy.create_deployer()
       assert_equal(true, deployer.is_a?(Caravan::DeployMethods::Shell))
     end
 
-    should "create shell deployer and deploy a file" do
-      deployer = Caravan::Deploy.create_deployer("shell")
-      assert_true(deployer.is_a?(Caravan::DeployMethods::Shell))
-      status = deployer.run(SOURCE_FOLDER, TARGET_FOLDER)
-      assert_equal(0, status)
-      assert_true(File.exist?("#{TARGET_FOLDER}/testfile"))
+    context "test shell deployer" do
+      setup do
+        Caravan::Command.run("mkdir -p #{TARGET_FOLDER}")
+      end
+
+      teardown do
+        Caravan::Command.run("rm -rf #{TARGET_FOLDER}")
+      end
+
+      should "create and deploy" do
+        deployer = Caravan::Deploy.create_deployer("shell")
+        assert_true(deployer.is_a?(Caravan::DeployMethods::Shell))
+        status = deployer.run(SOURCE_FOLDER, TARGET_FOLDER)
+        assert_equal(0, status)
+        assert_true(File.exist?("#{TARGET_FOLDER}/testfile"))
+      end
     end
 
-    should "create scp deployer" do
-      deployer = Caravan::Deploy.create_deployer("scp")
-      assert_true(deployer.is_a?(Caravan::DeployMethods::Scp))
-      status = deployer.run(SOURCE_FOLDER, TARGET_FOLDER)
-      assert_equal(0, status)
-      assert_true(File.exist?("#{TARGET_FOLDER}/testfile"))
+    context "test scp deployer" do
+      setup do
+        Caravan::Command.run("mkdir -p #{TARGET_FOLDER}")
+      end
+
+      teardown do
+        Caravan::Command.run("rm -rf #{TARGET_FOLDER}")
+      end
+
+      should "create and deploy" do
+        deployer = Caravan::Deploy.create_deployer("scp")
+        assert_true(deployer.is_a?(Caravan::DeployMethods::Scp))
+        status = deployer.run(SOURCE_FOLDER, TARGET_FOLDER)
+        assert_equal(0, status)
+        assert_true(File.exist?("#{TARGET_FOLDER}/testfile"))
+      end
     end
 
-    should "create rsync deployer" do
-      deployer = Caravan::Deploy.create_deployer("rsync")
-      assert_true(deployer.is_a?(Caravan::DeployMethods::Rsync))
-      status = deployer.run(SOURCE_FOLDER, TARGET_FOLDER)
-      assert_equal(0, status)
-      assert_true(File.exist?("#{TARGET_FOLDER}/testfile"))
+    context "test rsync deployer" do
+      setup do
+        Caravan::Command.run("mkdir -p #{TARGET_FOLDER}")
+      end
+
+      teardown do
+        Caravan::Command.run("rm -rf #{TARGET_FOLDER}")
+      end
+
+      should "create and deploy" do
+        deployer = Caravan::Deploy.create_deployer("rsync")
+        assert_true(deployer.is_a?(Caravan::DeployMethods::Rsync))
+        status = deployer.run(SOURCE_FOLDER, TARGET_FOLDER)
+        assert_equal(0, status)
+        assert_true(File.exist?("#{TARGET_FOLDER}/testfile"))
+      end
     end
 
-    should "create rsync local deployer" do
-      deployer = Caravan::Deploy.create_deployer("rsync_local")
-      assert_true(deployer.is_a?(Caravan::DeployMethods::RsyncLocal))
-      status = deployer.run(SOURCE_FOLDER, TARGET_FOLDER)
-      assert_equal(0, status)
-      assert_true(File.exist?("#{TARGET_FOLDER}/testfile"))
+    context "test rsync_local deployer" do
+      setup do
+        Caravan::Command.run("mkdir -p #{TARGET_FOLDER}")
+      end
+
+      teardown do
+        Caravan::Command.run("rm -rf #{TARGET_FOLDER}")
+      end
+
+      should "create and deploy" do
+        deployer = Caravan::Deploy.create_deployer("rsync_local")
+        assert_true(deployer.is_a?(Caravan::DeployMethods::RsyncLocal))
+        status = deployer.run(SOURCE_FOLDER, TARGET_FOLDER)
+        assert_equal(0, status)
+        assert_true(File.exist?("#{TARGET_FOLDER}/testfile"))
+      end
     end
 
     should "error if deployer not exists" do
