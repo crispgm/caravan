13de3fcf6885e50b0b873bf87b522e2d9da51667
David Zhang <crispgm@gmail.com>
2017-11-16 21:13:20 +0800

    Fix lib_test for create_listener

--- test/lib_test.rb
+++ test/lib_test.rb
@@ -5,7 +5,7 @@ class LibTest < CaravanTest
   context "caravan tests" do
     should "create a listen" do
       deployer = Caravan::Deploy.create_deployer(".", ".", "shell")
-      listener = Caravan.create_listener(deployer, ".", ".")
+      listener = Caravan.create_listener(deployer, ".")
       assert_equal(true, listener.is_a?(Listen::Listener))
     end
   end
