d716bbb2e1a3e71c758ee15d9ee65110f9ec178a
David Zhang <crispgm@gmail.com>
2017-11-16 21:05:57 +0800

    Shell deployer uses hooks now

--- lib/caravan/deploy_methods/shell.rb
+++ lib/caravan/deploy_methods/shell.rb
@@ -1,9 +1,39 @@
 module Caravan
   module DeployMethods
     class Shell < Caravan::Deploy::Base
+      def after_create
+        super do
+          Caravan::Message.info("Notice: Shell Deployer is like copy, mainly designed for test.")
+        end
+      end
+
+      def after_change
+        super do |m, a, r|
+          Caravan::Message.info("Hook: after_change")
+        end
+      end
+
+      def before_deploy
+        super do
+          Caravan::Message.info("Hook: before_deploy")
+        end
+      end
+
       def run
         super do |s, d|
           Caravan::Command.run("cp -r #{s} #{d}", @debug)
+        end
+      end
+
+      def after_deploy
+        super do
+          Caravan::Message.info("Hook: after_deploy")
+        end
+      end
+
+      def before_destroy
+        super do
+          Caravan::Message.info("Deployer destroyed")
         end
       end
     end
